f77abef50eeecf1e7d8d2bfed3fc270f096fa774
lolyping24 <dragomirlucas@gmail.com>
2025-11-22 17:19:48 +0200

    fix6

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, '/frontend/dist')));
  
-  app.get('*', (req, res) =>
+  app.get('/.*/', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
   );
 } else {
